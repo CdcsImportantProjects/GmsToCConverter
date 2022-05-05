7a1a71ee8dbb714a277dd5c968e50ff189f8710e
That one guy <96666783+CdcOnGitHub@users.noreply.github.com>
2022-05-05 13:28:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from tkinter import *
-
+from tkinter import filedialog as fd
 window = Tk()
+def OpenCS():
+    filetypes = (
+        ('CSharp Files', '*.cs'),
+        ('GameMakerLanguage Files', '*.gml'),
+        ('All files', '*.*')
+    )
+    filename = fd.askopenfilename(
+        filetypes=filetypes,
 
+    )
+    print(filename)
+    return filename
 window.title("GML To CSharp Converter")
-
+label = Label(window, text="Select a GML file")
+label.grid(column=0, row=0)
+btn = Button(window, text="Select file", command=lambda: print("File selected"))
+btn.grid(column=0,row=1)
+label2 = Label(window, text="Select a CS file")
+label2.grid(column=0, row=2)
+btn2 = Button(window, text="Select file", command=OpenCS)
+btn2.grid(column=0,row=3)
 window.mainloop()
